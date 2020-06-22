1d2f525f8bdc6c9345db50ed82916a203e9d2712
hbostann <hbostann@gmail.com>
2020-06-22 13:13:41 +0300

    Added basic API functions
    - Added Create Item
    - Added Delete Item
    - Added Update Item

--- gotodo.go
+++ gotodo.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -26,6 +28,84 @@ func IsAlive(response http.ResponseWriter, request *http.Request) {
 	log.Info("API is alive")
 	response.Header().Set("Content-Type", "application/json")
 	io.WriteString(response, `{"isAlive": true}`)
+}
+
+// ItemExists searches the DB for a ToDo item with a given id.
+// Returns true if found.
+func ItemExists(id int) bool {
+	todo := &TodoItem{}
+	result := db.First(todo, id)
+	if result.Error != nil {
+		log.Error("Todo item with given id not found in database", id)
+		return false
+	}
+	return true
+}
+
+// CreateTodo is the API endpoint to add a new todo item.
+func CreateTodo(response http.ResponseWriter, request *http.Request) {
+	description := request.FormValue("description")
+	log.WithField("description", description).Info("Adding todo item.")
+	todo := &TodoItem{Desc: description, Done: false}
+	db.Create(todo)
+	result := db.Last(todo)
+	response.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(response).Encode(result.Value)
+}
+
+// UpdateItem is the API endpoint to toggle the completion of a todo item
+func UpdateItem(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Error("Given value is not a valid id", vars["id"])
+		response.Header().Set("Content-Type", "application/json")
+		io.WriteString(response, `{"updated":false, "error": "Invalid Request"}`)
+		return
+	}
+	exists := ItemExists(id)
+	if exists == false {
+		response.Header().Set("Content-Type", "application/json")
+		io.WriteString(response, `{"updated":false, "error": "Record Not Found}`)
+		return
+	}
+	isDone, err := strconv.ParseBool(request.FormValue("done"))
+	if err != nil {
+		log.Error("Given todo id is not valid")
+		response.Header().Set("Content-Type", "application/json")
+		io.WriteString(response, `{"updated":false, "error": "Invalid Request"}`)
+		return
+	}
+	todo := &TodoItem{}
+	db.First(todo, id)
+	todo.Done = isDone
+	db.Save(todo)
+	response.Header().Set("Content-Type", "application/json")
+	io.WriteString(response, `{"updated": true}`)
+}
+
+// DeleteItem is the API endpoint for the deletion of a todo item.
+func DeleteItem(response http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Error("Given value %v, is not a valid id", vars["id"])
+		response.Header().Set("Content-Type", "application/json")
+		io.WriteString(response, `{"deleted":false, "error": "Invalid Request"}`)
+		return
+	}
+	exists := ItemExists(id)
+	if exists == false {
+		response.Header().Set("Content-Type", "application/json")
+		io.WriteString(response, `{"deleted":false, "error": "Record Not Found}`)
+		return
+	}
+	log.WithField("id", id).Info("Deleting todo")
+	todo := &TodoItem{}
+	db.First(todo, id)
+	db.Delete(todo)
+	response.Header().Set("Content-Type", "application/json")
+	io.WriteString(response, `{"deleted": true}`)
 }
 
 func init() {
